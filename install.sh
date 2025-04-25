09cc0c64b89e184544badff57cee74f4123fca04
DevOpsMaestro <unixmaestro@gmail.com>
2025-04-25 16:25:15 -0400

    chore: stripped names out and added networking back into the file

--- install.sh
+++ install.sh
@@ -12,25 +12,25 @@ printf "\n[1/5] Creating KinD cluster: $CLUSTER_NAME\n"
 cat <<EOF | kind create cluster --name "$CLUSTER_NAME" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+  ipFamily: ipv4
+  podSubnet: 10.244.0.0/16
+  serviceSubnet: 10.96.0.0/12
+  apiServerAddress: 127.0.0.1
+  apiServerPort: 6443
 nodes:
-- role: control-plane
-  image: "kindest/node:${K8S_VER}"
-  name: "cp-one"
-- role: control-plane
-  image: "kindest/node:${K8S_VER}"
-  name: "cp-two"
-- role: control-plane
-  image: "kindest/node:${K8S_VER}"
-  name: "cp-three"
-- role: worker
-  image: "kindest/node:${K8S_VER}"
-  name: "dp-one"
-- role: worker
-  image: "kindest/node:${K8S_VER}"
-  name: "dp-two"
-- role: worker
-  image: "kindest/node:${K8S_VER}"
-  name: "dp-three"
+  - role: control-plane
+    image: kindest/node:${K8S_VER}
+  - role: control-plane
+    image: kindest/node:${K8S_VER}
+  - role: control-plane
+    image: kindest/node:${K8S_VER}
+  - role: worker
+    image: kindest/node:${K8S_VER}
+  - role: worker
+    image: kindest/node:${K8S_VER}
+  - role: worker
+    image: kindest/node:${K8S_VER}
 EOF
 
 if ! command -v flux &> /dev/null; then
